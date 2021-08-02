cd4397089efcf2e43e3eb1ba3d8562f3ed51107e
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-02 11:31:25 +0800

    Update jd_dj_bean.js

--- jd_dj_bean.js
+++ jd_dj_bean.js
@@ -1,6 +1,5 @@
 /*
 京东到家鲜豆任务脚本,支持qx,loon,shadowrocket,surge,nodejs
-作者：passerby-b . thanks
 用抓包抓 https://daojia.jd.com/html/index.html 页面cookie填写到下面,暂时不知cookie有效期
 抓多账号直接清除浏览器缓存再登录新账号,千万别点退出登录,否则cookie失效
 cookie只要里面的deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx关键信息
@@ -15,6 +14,7 @@ TG群:https://t.me/passerbyb2021
 cron "10 0 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_bean.js,tag=京东到家鲜豆任务
 
 */
+
 
 const $ = new API("京东到家鲜豆任务");
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
@@ -50,11 +50,29 @@ let thiscookie = '', deviceid = '';
 
         deviceid = _uuid();
         let option = taskLoginUrl(deviceid, thiscookie);
+        //console.log(option)
+        
         await $.http.get(option).then(response => {
+           
+            hd = JSON.parse(JSON.stringify(response.headers));
+                let o2o_m_h5_sid = hd['set-cookie'][0]
+    o2o_m_h5_sid = o2o_m_h5_sid.substring(o2o_m_h5_sid.indexOf("=") + 1, o2o_m_h5_sid.indexOf(";"))
+    let PDJ_H5_JDPIN = hd['set-cookie'][1]
+    PDJ_H5_JDPIN = PDJ_H5_JDPIN.substring(PDJ_H5_JDPIN.indexOf("=") + 1, PDJ_H5_JDPIN.indexOf(";"))
+        let PDJ_H5_MOBILE = hd['set-cookie'][2]
+    PDJ_H5_MOBILE = PDJ_H5_MOBILE.substring(PDJ_H5_MOBILE.indexOf("=") + 1, PDJ_H5_MOBILE.indexOf(";"))
+        let PDJ_H5_PIN = hd['set-cookie'][3]
+    PDJ_H5_PIN = PDJ_H5_PIN.substring(PDJ_H5_PIN.indexOf("=") + 1, PDJ_H5_PIN.indexOf(";"))
+
+    cookie1 = "o2o_m_h5_sid=" + o2o_m_h5_sid + ";PDJ_H5_JDPIN=" + PDJ_H5_JDPIN + ";"+"PDJ_H5_MOBILE="+PDJ_H5_MOBILE+";"+"PDJ_H5_PIN="+PDJ_H5_PIN;
+
+    
+     
             let data = JSON.parse(response.body);
             if (data.code == 0) {
-                thiscookie = 'deviceid_pdj_jd=' + deviceid + '; PDJ_H5_PIN=' + data.result.PDJ_H5_PIN + '; o2o_m_h5_sid=' + data.result.o2o_m_h5_sid + ';';
-                //sid = data.result.o2o_m_h5_sid;
+               
+                thiscookie = thiscookie + cookie1;
+          
             }
             else thiscookie = 'aabbcc';
         });
@@ -77,6 +95,8 @@ let thiscookie = '', deviceid = '';
 }).finally(() => {
     $.done();
 })
+
+
 
 //个人信息
 async function userinfo() {
@@ -104,10 +124,13 @@ async function userinfo() {
 async function taskList() {
     return new Promise(async resolve => {
         try {
-            let option = urlTask('https://daojia.jd.com/client?_jdrandom=' + Math.round(new Date()) + '&functionId=task%2Flist&isNeedDealError=true&body=%7B%22modelId%22%3A%22M10001%22%2C%22plateCode%22%3A1%7D&channel=ios&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&traceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid, '');
+           // 'https://daojia.jd.com/client?_jdrandom=' + Math.round(new Date()) + '&_funid_=task/list&functionId=task/list&isNeedDealError=true&body={"modelId":"M10007","plateCode":4}&lat=29.540071&lng=114.06442&lat_pos=29.540071&lng_pos=114.06442&city_id=1458&channel=rn&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&traceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&_jdrandom=' + Math.round(new Date()) + '&_funid_=task/list'
+            
+            let option = urlTask('https://daojia.jd.com/client?_jdrandom=' + Math.round(new Date()) + '&_funid_=task/list&functionId=task/list&isNeedDealError=true&body={"modelId":"M10007","plateCode":4}&lat=29.540071&lng=114.06442&lat_pos=29.540071&lng_pos=114.06442&city_id=1458&channel=rn&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&traceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&_jdrandom=' + Math.round(new Date()) + '&_funid_=task/list', '');
 
             $.http.get(option).then(response => {
                 var data = JSON.parse(response.body);
+               // console.log(option);
                 //console.log(response.body);
                 resolve(data);
             })
@@ -199,8 +222,9 @@ function urlTask(url, body) {
 }
 
 function taskLoginUrl(deviceid, thiscookie) {
+  // 'https://daojia.jd.com/client?_jdrandom=' + (+new Date()) + '&_funid_=login/treasure&functionId=login/treasure&body={}&lat=&lng=&lat_pos=&lng_pos=&city_id=&channel=h5&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&isNeedDealError=false&traceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&_jdrandom=1627648796622&_funid_=login/treasure'
     return {
-        url: 'https://daojia.jd.com/client?_jdRandom=' + (+new Date()) + '&functionId=xapp/loginByPtKeyNew&body=' + escape(JSON.stringify({ "fromSource": 5, "businessChannel": 150, "subChannel": "", "regChannel": "" })) + 'channel=ios&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&code=011UYn000apwmL1nWB000aGiv74UYn03&deviceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceModel=appmodel',
+        url: 'https://daojia.jd.com/client?_jdrandom=' + (+new Date()) + '&_funid_=login/treasure&functionId=login/treasure&body={}&lat=&lng=&lat_pos=&lng_pos=&city_id=&channel=h5&platform=6.6.0&platCode=h5&appVersion=6.6.0&appName=paidaojia&deviceModel=appmodel&isNeedDealError=false&traceId=' + deviceid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&_jdrandom=1627648796622&_funid_=login/treasure',
         headers: {
             "Cookie": 'deviceid_pdj_jd=' + deviceid + ';' + thiscookie + ';',
             "Host": "daojia.jd.com",
