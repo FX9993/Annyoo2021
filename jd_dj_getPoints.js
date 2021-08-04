7a142381b542c8698f67db9e8a556b2b7b5a0d1d
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-08-04 21:28:23 +0800

    Update jd_dj_getPoints.js

--- jd_dj_getPoints.js
+++ jd_dj_getPoints.js
@@ -14,7 +14,7 @@
 //[Script]
 //cron "*/5 * * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_getPoints.js,tag=京东到家鲜豆庄园收水滴
 
-const $ = new API("京东到家鲜豆庄园收水滴");
+const $ = new API("jd_dj_getPoints");
 
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 let cookies = [];
